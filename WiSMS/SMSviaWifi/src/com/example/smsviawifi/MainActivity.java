88a459d389efab6da2c1ce90ab24f21e44ffbe08
Sahan Bulathsinhala <sahan.bsb@gmail.com>
2015-08-22 16:19:37 +0530

    SMS class added

--- WiSMS/SMSviaWifi/src/com/example/smsviawifi/MainActivity.java
+++ WiSMS/SMSviaWifi/src/com/example/smsviawifi/MainActivity.java
@@ -1,20 +1,30 @@
 package com.example.smsviawifi;
 
+import java.math.BigInteger;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.nio.ByteOrder;
+
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
 import android.view.Menu;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	
+	Net n = new Net();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Net n = new Net();
         n.start();
         TextView t = (TextView) findViewById(R.id.textview1);
-        t.setText("Done");
+        String ip = wifiIpAddress(this);
+        t.setText("Goto http://"+ip+":8080");
     }
 
 
@@ -23,6 +33,41 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    public void onDestroy(){
+    	//n.terminate();
+    	try {
+			n.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			Log.d("WIFISMS", e.toString());
+		}
+    	Log.d("WIFISMS", "Main Terminated");
+    	
+    	super.onDestroy();
+    }
+    
+    protected String wifiIpAddress(Context context) {
+        WifiManager wifiManager = (WifiManager) context.getSystemService(WIFI_SERVICE);
+        int ipAddress = wifiManager.getConnectionInfo().getIpAddress();
+
+        // Convert little-endian to big-endianif needed
+        if (ByteOrder.nativeOrder().equals(ByteOrder.LITTLE_ENDIAN)) {
+            ipAddress = Integer.reverseBytes(ipAddress);
+        }
+
+        byte[] ipByteArray = BigInteger.valueOf(ipAddress).toByteArray();
+
+        String ipAddressString;
+        try {
+            ipAddressString = InetAddress.getByAddress(ipByteArray).getHostAddress();
+        } catch (UnknownHostException ex) {
+            Log.e("WIFIIP", "Unable to get host address.");
+            ipAddressString = null;
+        }
+
+        return ipAddressString;
     }
     
 }
---
+++ WiSMS/SMSviaWifi/src/com/example/smsviawifi/SMS.java
@@ -0,0 +1,44 @@
+package com.example.smsviawifi;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
+import android.util.Log;
+import android.widget.Toast;
+
+public class SMS extends BroadcastReceiver {
+
+	final SmsManager sms = SmsManager.getDefault();
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// Retrieves a map of extended data from the intent.
+		final Bundle bundle = intent.getExtras();
+		try {
+			if (bundle != null) {
+				final Object[] pdusObj = (Object[]) bundle.get("pdus");
+				for (int i = 0; i < pdusObj.length; i++) {
+					SmsMessage currentMessage = SmsMessage
+							.createFromPdu((byte[]) pdusObj[i]);
+					String phoneNumber = currentMessage
+							.getDisplayOriginatingAddress();
+					String senderNum = phoneNumber;
+					String message = currentMessage.getDisplayMessageBody();
+					Log.i("WIFISMS", "senderNum: " + senderNum
+							+ "; message: " + message);
+					// Show Alert
+					int duration = Toast.LENGTH_LONG;
+					Toast toast = Toast.makeText(context, "senderNum: "
+							+ senderNum + ", message: " + message, duration);
+					toast.show();
+				} // end for loop
+			} // bundle is null
+		} catch (Exception e) {
+			Log.e("WIFISMS", "Exception smsReceiver" + e);
+		}
+	}
+
+}
